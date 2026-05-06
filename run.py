a972c5d82879721f6397524b961702f57ca9c129
jelicpat <patrik.jelic@uni-a.de>
2026-05-06 14:18:25 +0200

    Make script updated

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import subprocess 
+import argparse
 
 def run_command(command):
     try:
@@ -16,8 +17,25 @@ def run_command(command):
 
 create_dir = "cmake -B build"
 make_build = "cmake --build build"
+create_release_dir = "cmake -S . -B build -DCMAKE_BUILD_TYPE=Release"
+make_release_build = "cmake --build build"
 
 if __name__=="__main__":
-    run_command(create_dir)
-    run_command(make_build)
     
+    parser = argparse.ArgumentParser(
+        description="CMake -- Mandelbrot Generator"
+    )
+    parser.add_argument(
+        "mode",
+        default="debug",
+        choices=["debug", "release"],
+    )
+    
+    args = parser.parse_args()
+    
+    if args.mode == "debug":
+        run_command(create_dir)
+        run_command(make_build)
+    elif args.mode == "release":
+        run_command(create_release_dir)
+        run_command(make_release_build)
