4e32aee29110690eacf753b55e63c48106f72542
jelicpat <patrik.jelic@uni-a.de>
2026-05-06 08:54:42 +0200

    Deleted comments

--- src/filedialog.cpp
+++ src/filedialog.cpp
@@ -1,6 +1,5 @@
 #include "filedialog.hpp"
 
-// 1. Include PFD here (it will include windows.h internally)
 #include "portable-file-dialogs.h"
 #include <filesystem>
 
@@ -12,18 +11,12 @@ std::vector<std::string> AskForDirectoryFilename()
 
     if (!selection.empty())
     {
-        // 2. Convert the string path to a filesystem path object
         std::filesystem::path fullPath = selection[0];
 
-        // 3. Extract the filename without the extension
-        // Example: "C:/Users/Docs/data.txt" -> "data"
         std::string filename = fullPath.stem().string();
 
-        // Optional: Extract the directory location
         std::string directory = fullPath.parent_path().string();
         return {directory, filename};
-        // ig.setPath(directory, filename);
-        // ig.generate();
     }
 
     return {};
